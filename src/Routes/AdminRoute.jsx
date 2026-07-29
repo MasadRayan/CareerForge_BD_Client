0c0246405de466f70d2d34a43496f64a7edd918d
Masad Rayan <masadrayan2002@gmail.com>
2026-07-30 03:51:29 +0600

    feat: implement user role management with useUserRole hook; enhance AdminRoute and UserRoute for loading states and role-based navigation

--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -1,10 +1,21 @@
+import useUserRole from '../Hooks/useUserRole';
+import useAuth from '../Hooks/useAuth';
+import { Navigate } from 'react-router-dom';
+import LoadingSpinner from '../Components/LoadingSpinner/LoadingSpinner';
 
-const AdminRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const AdminRoute = ({ children }) => {
+    const { role, roleLoading } = useUserRole();
+    const { user, loading } = useAuth();
+
+    if (loading || roleLoading) {
+        return <LoadingSpinner />;
+    }
+
+    if (!user || role !== 'admin') {
+        return <Navigate to="/forbidden" />;
+    }
+
+    return children;
 };
 
 export default AdminRoute;
