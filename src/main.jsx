dc4af5f26676fe98b76d9c5b64dfeaf5dd41665f
Masad Rayan <masadrayan2002@gmail.com>
2026-07-30 04:23:11 +0600

    feat: enhance dashboard layout with user and admin navigation; integrate useUserRole for role-based rendering

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,19 @@ import './index.css'
 import { RouterProvider } from "react-router-dom";
 import router from './router/router.jsx'
 import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ThemeProvider from "./Context/ThemeProvider";
 import AuthProvider from './Context/AuthProvider.jsx'
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
-   <ThemeProvider>
-      <AuthProvider>
-         <RouterProvider router={router}></RouterProvider>
-         <Toaster />
-      </AuthProvider>
-   </ThemeProvider>
+   <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+         <AuthProvider>
+            <RouterProvider router={router}></RouterProvider>
+            <Toaster />
+         </AuthProvider>
+      </ThemeProvider>
+   </QueryClientProvider>
 )
