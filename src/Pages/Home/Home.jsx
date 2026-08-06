a4e482c20a826f6ae1cf9b04578bf72d1b6af9fa
Masad Rayan <masadrayan2002@gmail.com>
2026-08-06 16:06:11 +0600

    feat: add AnalysisCV and PricingSection components to Home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,8 @@
+import AnalysisCV from "../Features/AnalysisCV";
 import FAQ from "../Features/FAQ";
 import Features from "../Features/Features";
 import HowItWorks from "../Features/HowItWorks";
+import PricingSection from "../Features/PricingSection";
 import Stats from "../Features/Stats";
 import Banner from "./Banner";
 
@@ -8,9 +10,11 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <AnalysisCV></AnalysisCV>
             <Stats></Stats>
             <HowItWorks></HowItWorks>
             <Features></Features>
+            <PricingSection></PricingSection>
             <FAQ></FAQ>
         </div>
     );
